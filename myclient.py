3ff39700ca26f2e4f09a7a69cd20e7e7b166c239
petrov-anna <anna.petrov-petrov@yandex.ru>
2021-03-10 14:09:35 +0300

    Клиент выводит в консоль служебные сообщения

--- myclient.py
+++ myclient.py
@@ -2,12 +2,15 @@ import socket
 
 sock = socket.socket()
 sock.connect(('localhost', 9090))
+print(f'We have connected to the server')
 
 while True:
     message = input()
     sock.send(message.encode())
+    print('Sending this message')
     data = sock.recv(1024)
-    print(data.decode())
+    print(f'Receiving a message => {data.decode()}')
     if message == 'exit':
+        print(f'We have been disconnected to the server')
         sock.close()
         break
